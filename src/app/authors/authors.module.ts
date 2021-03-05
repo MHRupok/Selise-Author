07a3096f76858519f1688a1be3a24c4c5a7f7156
Rupok <mehedi.hassan@gigatechltd.com>
2021-03-05 20:36:32 +0600

    first working

--- src/app/authors/authors.module.ts
+++ src/app/authors/authors.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { AuthorsRoutingModule } from './authors-routing.module';
 import { AuthorListComponent } from './author-list/author-list.component';
 import { FavouriteListComponent } from './favourite-list/favourite-list.component';
-import { ListItemComponent } from '../components/shared/list-item/list-item.component';
 import { ListItemsComponent } from '../components/shared/list-items/list-items.component';
 
 
